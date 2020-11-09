d97301fa1a1aa210fdbafb94c71d4c9d265bbb90
Alex <cigan@ua.fm>
2020-11-09 17:02:56 +0200

    add blocks with a menu from the database

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {MainPage, CartPage} from '../pages';
+import { MainPage, CartPage } from '../pages';
 import AppHeader from '../app-header';
+import { Route, Router, Switch } from 'react-router-dom';
 
 import Background from './food-bg.jpg';
 
 const App = () => {
     return (
-        <div style={{background: `url(${Background}) center center/cover no-repeat`}} className="app">
-            <AppHeader total={50}/>
-            <MainPage/>
-            <CartPage/>
+        <div style={{ background: `url(${Background}) center center/cover no-repeat` }} className="app">
+            <AppHeader total={50} />
+            <Switch>
+                <Route
+                    path='/'
+                    component={MainPage}
+                    exact />
+
+                <Route
+                    path='/cart'
+                    component={CartPage} />
+            </Switch>
         </div>
     )
 }
